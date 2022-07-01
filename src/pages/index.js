b15eea69742d3ae50f141d93987801358fef7db3
Alejandro Venegas <avenegas@apexsystems.com>
2022-07-01 15:16:10 -0500

    Spiderman image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from 'gatsby'
+import { StaticImage } from 'gatsby-plugin-image'
 
 import Layout from "../components/layout"
 
@@ -9,6 +9,10 @@ const IndexPage = () => {
     <main>
       <Layout pageTitle="Home Page">
         <p>I'm making this by following the Gatsby Tutorial.</p>
+        <StaticImage
+          alt="Spiderman"
+          src="../images/spiderman.jpg"
+        />
       </Layout>
     </main>
   )
