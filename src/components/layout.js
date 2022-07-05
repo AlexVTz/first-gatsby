f9e533191931c3f3c49e556ef829983e7206a5d3
Alejandro Venegas <avenegas@apexsystems.com>
2022-07-04 21:16:26 -0500

    Gatsby basics completed

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,29 @@
 import * as React from 'react'
-import { Link } from 'gatsby'
-import { container, navLinks, navLinkItem, navLinkText, heading } from './layout.module.css'
+import { Link, graphql, useStaticQuery } from 'gatsby'
+import { container, navLinks, navLinkItem, navLinkText, heading, siteTitle } from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
+
+
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `)
+
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <title>{data.site.siteMetadata.title}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}><Link to="/" className={navLinkText}>Home</Link></li>
           <li className={navLinkItem}><Link to="/about" className={navLinkText}>About</Link></li>
+          <li className={navLinkItem}><Link to="/blog" className={navLinkText}>Blog</Link></li>
         </ul>
       </nav>
       <main>
